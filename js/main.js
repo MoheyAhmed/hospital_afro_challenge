1342c644b4fb406eed2fe8d9822b4b890b30d8aa
Mohey Ahmed <90425906+MoheyAhmed@users.noreply.github.com>
2023-12-31 13:39:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+// var width = window.innerWidth;
+
+$("#bar_icon").click(function(){
+    $(".second_nav_links").slideToggle(500)
+    
+})
+
+
+// if(width>850){
+//     document.getElementById("second_nav_links").style.display = "none !important"
+// }
+
+// document.getElementById("bar_icon").addEventListener("click" , function(){
+//     document.getElementById("second_nav_links").style.display = "block"
+// })
+
+// document.getElementById("bar_icon").addEventListener("click" , function(){
+//     document.getElementById("second_nav_links").style.display = "block"
+// })
